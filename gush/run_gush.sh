85bc6d05c94acb761e279386ed8f0cfa9aabf958
Bogdan <metallica127@mail.ru>
2017-10-24 13:51:21 +0300

    run_gush fx

--- gush/run_gush.sh
+++ gush/run_gush.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
-#gex_env='main'
-gex_env=$1
+gex_env="${gex_env:-$1}"
+
+[[ -z "${gex_env}" ]] && {
+    echo -e "${RED}gex_env is unset${NC}";
+    echo -e "Usage: ${YELLOW}./run_gush \$gex_env ${NC}"
+    exit 1
+}
+
 n_threads=10
 queue='provisiongush'
 
 
-_gex_env=${gex_env} bundle exec sidekiq -r ./Gushfile.rb -c ${n_threads} -q ${queue} -e development -v
+gex_env=${gex_env} bundle exec sidekiq -r ./Gushfile.rb -c ${n_threads} -q ${queue} -e development -v
