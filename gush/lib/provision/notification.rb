6871263ea080ca188f76ac7b505fd61654713540
Bogdan <metallica127@mail.ru>
2017-10-25 17:33:37 +0300

    sidekiq notifier

---
+++ gush/lib/provision/notification.rb
@@ -0,0 +1,23 @@
+module Provision
+  class Notification
+
+    def self.notify_api_sidekiq(event, data)
+
+      res = Sidekiq::Client.push(
+          'queue' => 'provisionnotify', 'retry' => false,
+          'class' => 'ProvisionNotificationsWorker',
+          'args' => [{event: event, data: data}]
+      )
+
+      if res
+        #TODO: log notify sent
+        puts "#{event} notification sent. job_id: #{res}"
+      else
+        #TODO: log notify fail
+        puts "#{event} notification for #{data} failed."
+      end
+
+    end
+
+  end
+end
